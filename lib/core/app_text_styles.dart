43eb5d14bd51cc91298109c86d39b3bedacd30c3
Wagner.Araujo <araujowp@gmail>
2021-05-01 16:18:09 -0300

    add internacionalização intl

---
+++ lib/core/app_text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppStyle{
+
+  static final TextStyle transactionText = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.purple
+  ); 
+
+  static final TextStyle transactionDate  = TextStyle(
+    //fontWeight: FontWeight.bold, 
+    color: Colors.grey
+  );
+
+  static final TextStyle transactionTitle = TextStyle(
+    fontSize: 16,
+    color: Colors.black
+  );
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+
+import 'package:expenses/core/app_text_styles.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -13,17 +16,48 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHome extends StatelessWidget {
-
-  final _transacions  = [
-    Transaction(id: 't1', title: 'Acougue maminha e margarina', value: 65.72, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de luz ', value: 223.17, date: DateTime.now())
+  final _transacions = [
+    Transaction(
+        id: 't1',
+        title: 'Acougue',
+        value: 65.72,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de luz ', value: 223.17, date: DateTime.now())
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Despesas'),),
-      body: Center(child: Text('oi oi oi ')), 
-    );
+        appBar: AppBar(
+          title: Text('Despesas'),
+        ),
+        body: Column(
+          children: _transacions.map((tr) {
+            return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2 
+                      )
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text('R\$ ${tr.value.toStringAsFixed(2)}', style: AppStyle.transactionText, )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(tr.title, style: AppStyle.transactionTitle,),
+                      Text(DateFormat('d MMM y').format(tr.date), style: AppStyle.transactionDate,),
+                    ],
+                  )
+                ],
+              ),
+            );
+          }).toList(),
+        ));
   }
 }
