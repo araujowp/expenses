3b568114c2252c179239a2881303d489af7c1864
Wagner.Araujo <araujowp@gmail>
2021-05-12 19:13:20 -0300

    formulario modal

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,56 +1,62 @@
 // ignore: avoid_web_libraries_in_flutter
 
-import 'package:expenses/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class TransacionForm extends StatelessWidget {
-
-  final titleControler = TextEditingController();
-  final valueControler = TextEditingController();
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
   final void Function(String, double) onSubmit;
 
-  TransacionForm(this.onSubmit);
+  TransactionForm(this.onSubmit);
+
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-          child: Card(
-              elevation: 5,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: titleControler,
-                          decoration: InputDecoration(labelText: 'Titulo'),
-                        ),
-                        TextField(
-                          controller: valueControler,
-                          decoration: InputDecoration(labelText: 'Valor (R\$)'),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            TextButton(
-                              style: TextButton.styleFrom(primary: Colors.purple,),
-                              child: Text('Nova Transacao '),
-                              onPressed: () {
-                                final title = titleControler.text;
-                                final value = double.tryParse(valueControler.text) ?? 0.0;
-                                onSubmit(title, value);
-                              },
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  )
-                ],
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Título',
               ),
             ),
+            TextField(
+              controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Valor R\$',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Nova Transação'),
+                  textColor: Colors.purple,
+                  onPressed: _submitForm,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -3,52 +3,66 @@ import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class TransacionList extends StatelessWidget {
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
 
-  final List<Transaction> transacions;
-
-  TransacionList(this.transacions); 
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
       child: ListView.builder(
-        itemCount: transacions.length,//o listview não reenderiza a lista toda deste modo apenas oque precisa 
-        itemBuilder: (ctx,index){
-          final tr = transacions[index];
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
           return Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin:
-                        EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2)),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: AppStyle.transactionText,
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tr.title,
-                        style: AppStyle.transactionTitle,
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: AppStyle.transactionDate,
-                      ),
-                    ],
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
                   ),
-                ],
-              ),
-            );
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tr.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('d MMM y').format(tr.date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
         },
-        ),
+      ),
     );
   }
 }
